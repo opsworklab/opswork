fc259deb26f00abb70a9c0f0eea703fd2b1b8cb3
tech4target <36736546+opsworklab@users.noreply.github.com>
2018-09-15 22:22:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html lang="en">
 
     <head>
-        <title>Hello Qshore Students</title>
+        <title>Hello AWS Students</title>
     </head>
 
     <body>
 <div align="center">
-                <h1>Hello Qshore Students's</h1>
+                <h1>Hello AWS Students's</h1>
                 <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
-                <div align="center"><img src="https://s3.eu-west-2.amazonaws.com/opsworklab/amazon-logo.jpg"></div>
-                <p>You Qshore cloud Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <div align="center"><img src="https://s3-us-west-1.amazonaws.com/brijbucket/HappyFace.jpg"></div>
+                <p>You first cloud Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
     </body>
